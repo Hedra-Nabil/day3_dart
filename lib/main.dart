9581acb7facfb99dda992eaaa8aee2b8a448f5e5
Hedra Nabil <hedranabil614@gmail.com>
2025-05-29 15:04:28 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:day3_dart/features/social%20media/social_media.dart';
-import 'package:day3_dart/features/transactions/transactions.dart';
+// import 'package:day3_dart/features/social%20media/social_media.dart';
+// import 'package:day3_dart/features/transactions/transactions.dart';
 
 void main(List<String> arguments) {
   //BankAccount bankAccount = BankAccount();
@@ -8,11 +8,89 @@ void main(List<String> arguments) {
   // bankAccount.getBalance;
   // bankAccount.withdraw = 200;
   // bankAccount.getBalance;
-  UiRendering uiRendering = UiRendering(SocialMediaImplementation());
-  uiRendering.showPostsButton();
-  uiRendering.deletePostsButton(0);
-  uiRendering.showPostsButton();
-  uiRendering.addPostsButton('hedra');
-  uiRendering.showPostsButton();
+  // UiRendering uiRendering = UiRendering(SocialMediaImplementation());
+  // uiRendering.showPostsButton();
+  // uiRendering.deletePostsButton(0);
+  // uiRendering.showPostsButton();
+  // uiRendering.addPostsButton('hedra');
+  // uiRendering.showPostsButton();
+  // Test test = Test();
+  // test.printNum(0);
+  // test.printNum1(num: 1);
+  // test.printNum2(2);
+  // test.printNum3(num: 3);
+
+  // mohmmed m = mohmmed();
+  // m.Name();
+  // m.Id();
+  // m.calculate();
+  //'hedra'.printString;
+  // BankAccount bankAccount = BankAccount();
+  // BankAccount bankAccount2 = BankAccount();
+  // print(BankAccount.balance);
+  // bankAccount.deposit(5000);
+  // print(BankAccount.balance);
+  // bankAccount.withdraw(1000);
+  // print(BankAccount.balance);
+  // print('bank acount 2:${BankAccount.balance}');
 }
 
+class person {
+  void name() {}
+  void type() {}
+}
+
+class Egyptian {}
+// class BankAccount {
+//   static double balance = 0;
+//   void deposit(int count) {
+//     balance += count;
+//   }
+
+//   void withdraw(int count) {
+//     balance -= count;
+//   }
+// }
+
+// extension  on String {
+//   void printString() {
+//     print(this);
+//   }
+// }
+// mixin Hr {
+//   String? name;
+//   void calculate() {
+//     print('total employees');
+//   }
+// }
+
+// class Employee {
+//   void Name() {
+//     print('name');
+//   }
+
+//   void Id() {
+//     print('id');
+//   }
+// }
+
+// class mohmmed extends Employee with Hr {
+
+// }
+// class Test {
+//   void printNum(int num) {
+//     print(num);
+//   }
+
+//   void printNum1({required int num}) {
+//     print(num);
+//   }
+
+//   void printNum2([int? num]) {
+//     print(num);
+//   }
+
+//   void printNum3({int? num}) {
+//     print(num);
+//   }
+// }
