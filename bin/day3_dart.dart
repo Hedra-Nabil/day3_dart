a4c155e58e358290f5ca9d63983f3ee9bb01bde0
Hedra Nabil <hedranabil614@gmail.com>
2025-05-29 11:39:42 +0300

    first commit

---
+++ bin/day3_dart.dart
@@ -0,0 +1,5 @@
+import 'package:day3_dart/day3_dart.dart' as day3_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day3_dart.calculate()}!');
+}
---
+++ test/day3_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:day3_dart/day3_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
