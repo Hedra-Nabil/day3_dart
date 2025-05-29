734e6fc783281d5633ec6caae3b65101143981e0
Hedra Nabil <hedranabil614@gmail.com>
2025-05-29 12:03:49 +0300

    .

--- bin/day3_dart.dart
+++ bin/day3_dart.dart
@@ -1,5 +1,4 @@
-import 'package:day3_dart/day3_dart.dart' as day3_dart;
+import 'package:day3_dart/main.dart' as day3_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${day3_dart.calculate()}!');
-}
+//qq
+void main(List<String> arguments) {}
--- test/day3_dart_test.dart
+++ test/day3_dart_test.dart
@@ -1,8 +1,4 @@
-import 'package:day3_dart/day3_dart.dart';
+import 'package:day3_dart/main.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
